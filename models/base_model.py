ab78dab13525cc2928400077f364ec88e34f5a68
Alfred Tuva <muindealfred01@gmail.com>
2021-06-26 11:59:38 +0300

    First commit

---
+++ models/base_model.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+'''
+Contains all the Base model for all the other classes
+'''
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    '''
+    The base class for all the other classes
+    '''
+
+    def __init__(self, *args, **kwargs):
+        '''
+        Initializes the base model
+        '''
+        if kwargs:
+            kwargs.pop("__class__")
+            for arg, value in kwargs.items():
+                if arg == "created_at" or arg == "updated_at":
+                    date_obj = value.isoformat()
+                    setattr(self, arg, date_obj)
+                else:
+                    setattr(self, arg, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+
+    def __str__(self):
+        '''
+        Returns string representation of the object
+        '''
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        '''
+        Saves an instance
+        '''
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        '''
+        Returns a dictionary containing a representation of the instance
+        '''
+        obj_dict = {}
+        obj_dict['__class__'] = self.__class__.__name__
+        for k, v in self.__dict__.items():
+            if k == "created_at" or k == "updated_at":
+                date_str = v.isoformat()
+                obj_dict[k] = date_str
+            else:
+                obj_dict[k] = v
+        return obj_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+'''
+Contains the file storage class
+'''
+import json
+import os
+
+
+class FileStorage:
+    '''
+    The file storage class
+    attributes:
+        __file_path: A path to the json file
+        __objects: A dictionary to store all objects
+    '''
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        '''
+        Returns the dictionary __objects
+        '''
+        return self.__objects
+
+    def new(self, obj):
+        '''
+        Adds to __objects a new object
+        '''
+        FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+
+    def save(self):
+        '''
+        serializes __objects to the JSON file (path: __file_path)
+        '''
+        #serialize the dictionary
+        json_dict = {}
+        for k, obj in FileStorage.__objects.items():
+            json_dict[k] = obj.to_dict()
+
+        with open(__file_path, "w", encoding="utf-8") as f:
+            f.write(json.dumps(json_dict))
+
+    def reload(self):
+        '''
+        deserializes the JSON file to __objects
+        '''
+        if os.path.isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, mode="r",encoding="utf-8") as f:
+
+
+
+            
+
+
+
