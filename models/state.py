c4dbcb5468a1662aad707bb7267bb2a86920691d
Alfred Tuva <muindealfred01@gmail.com>
2021-06-28 13:28:35 +0300

    Add the rest of the modesl

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Contains the State Model
+'''
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    '''
+    A model for the State
+    Attributes:
+        name (str): The name of the State
+    '''
+    name = ""
