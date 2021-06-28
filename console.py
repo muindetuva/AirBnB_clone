237a90ecc72d3cee37fb96973cebf29da5a11037
Millennial-Polymath <muchamingotho@gmail.com>
2021-06-28 10:54:11 +0000

    Update to include other classes

--- console.py
+++ console.py
@@ -6,7 +6,11 @@ from models.base_model import BaseModel
 from models.user import User
 from models.__init__ import storage
 import cmd
-
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
