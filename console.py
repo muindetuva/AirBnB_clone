9c5c0668c3771d182c7334389dc9c4851438e221
Alfred Tuva <muindealfred01@gmail.com>
2021-06-28 10:26:18 +0300

    Allow console to handle other classes

--- console.py
+++ console.py
@@ -10,7 +10,7 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
-    __classes = ["BaseModel", "User"]
+    __classes = {"BaseModel": BaseModel, "User": User}
     # This is a dictionary of attributes and their values so that we can
     # properly parse them during the upda
     __types = {"age": int}
@@ -34,7 +34,7 @@ class HBNBCommand(cmd.Cmd):
         elif arg not in HBNBCommand.__classes:
             print("** class does not exist **")
         else:
-            model_1 = BaseModel()
+            model_1 = HBNBCommand.__classes[arg]()
             model_1.save()
             print(model_1.id)
 
