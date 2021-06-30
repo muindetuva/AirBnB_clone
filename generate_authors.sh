1b99d85d7febb1c64ac57cdb1a2d0c7dd5e00bd8
Muchami Ngotho <53700964+Millennial-Polymath@users.noreply.github.com>
2021-06-30 22:41:54 +0300

    Delete generate_authors.sh

--- generate_authors.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "# This file lists all individuals having contributed content to the repository." > AUTHORS
-echo "" >> AUTHORS
-git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf >> AUTHORS
